89843a13d88029dfc9a07e4c2fef9c31871cae7e
Cho <mandoo020374@gmail.com>
2016-08-28 19:48:06 +0900

    심플채팅

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,5 +3,22 @@ class ChatController < ApplicationController
   end
 
   def new
+    @ask = params[:ask]
+  end
+
+  def create
+    Chat.create(is_man: true, content: params[:content])
+    dic = Dictionary.where(ask: params[:content]).sample
+    if dic.nil?
+      redirect_to "/chat/new?ask=#{URI.encode(params[:content])}"
+    else
+      Chat.create(is_man: false, content: dic.answer)
+      redirect_to '/chat/index'
+    end
+  end
+
+  def learn
+    Dictionary.create(ask: params[:ask], answer: params[:answer])
+    redirect_to '/chat/index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'chat/index'
 
   get 'chat/new'
+  get 'chat/learn'
+  get 'chat/create'
+  root 'chat#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
